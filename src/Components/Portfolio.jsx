ed02e95e02d956e89d8fa8aaeea010b4e42838ae
Dalton A. Schmidt <daltonaschmidt@gmail.com>
2025-05-10 16:28:06 +0000

    "Tried to fix file path issue in Portfolio.jsx"

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -20,6 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
+
+import { projectImages } from "../Assets/images/images.js";
+
 import image from "../Assets/images/milkyway.jpg";
 
 const imageAltText = "Photography of the California Nebula - Steve Busch (Unsplash)";
